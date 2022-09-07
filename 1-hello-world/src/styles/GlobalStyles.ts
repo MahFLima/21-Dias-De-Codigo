1bd5aaf198b148c6910e1ce8ad696f585b562c71
Mariana Lima <mariana.lima118@outlook.com>
2022-09-07 17:10:11 -0300

    update hello world

--- 1-hello-world/src/styles/GlobalStyles.ts
+++ 1-hello-world/src/styles/GlobalStyles.ts
@@ -5,11 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    color: var(--text-color);
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-  body{
-    background-image: url('/assets/background.png');
-    overflow-x: hidden;
+    color: white;
   }
 `;
