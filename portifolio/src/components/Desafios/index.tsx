327b1f7529992643f80d515925e61d0f3b5a39cb
Mariana Ferreira Lima <mariana.lima118@outlook.com>
2023-04-03 14:03:01 -0300

    outros projetos

--- portifolio/src/components/Desafios/index.tsx
+++ portifolio/src/components/Desafios/index.tsx
@@ -3,23 +3,40 @@ import data from "../../data"
 import Card from '../Card';
 import { Container } from './styles';
 
+const projects = [data[0], data[3], data[8], data[14], data[17]]
+
 const Desafios: React.FC = () => {
 
   return (
     <Container id="plus">
-      <strong>21 Dias De Codigo</strong>
+      <strong>Outros Projetos</strong>
       <div>
-
-        {data.map((item, index) => {
-          return(
-            <Card 
-              key={index} 
-              title={item.title} 
-              image={item.image} 
-              link={item.link}
+        
+            <Card
+              title={data[0].title} 
+              image={data[0].image} 
+              link={data[0].link}
             />
-          )
-        })}
+            <Card
+              title={data[3].title} 
+              image={data[3].image} 
+              link={data[3].link}
+            />
+            <Card
+              title={data[8].title} 
+              image={data[8].image} 
+              link={data[8].link}
+            />
+           <Card
+              title={data[14].title} 
+              image={data[14].image} 
+              link={data[14].link}
+            />
+            <Card
+              title={data[17].title} 
+              image={data[17].image} 
+              link={data[17].link}
+            />
         
       </div>
     </Container>
